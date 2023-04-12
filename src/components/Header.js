d3cdd21806390970932669473cf02bbdbc5bb18b
CrioUser <crio-user@criodo.com>
2023-04-12 15:42:39 +0530

    “ME_QKART_FRONTEND_V2_MODULE_LOGIN”

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,22 +3,55 @@ import { Avatar, Button, Stack } from "@mui/material";
 import Box from "@mui/material/Box";
 import React from "react";
 import "./Header.css";
-
+import { useHistory } from "react-router-dom";
 const Header = ({ children, hasHiddenAuthButtons }) => {
-    return (
-      <Box className="header">
-        <Box className="header-title">
-            <img src="logo_light.svg" alt="QKart-icon"></img>
-        </Box>
+  const history = useHistory();
+  const userName = localStorage.getItem("username");
+  return (
+    <Box className="header">
+      <Box className="header-title">
+        <img src="logo_light.svg" alt="QKart-icon"></img>
+      </Box>
+      <Box>{children}</Box>
+      {hasHiddenAuthButtons ? (
         <Button
           className="explore-button"
           startIcon={<ArrowBackIcon />}
           variant="text"
+          onClick={() => history.push("/")}
         >
-          Back to explore
+          back to explore
         </Button>
-      </Box>
-    );
+      ) : (
+        <Stack direction="row" spacing={1} alignItems="center">
+          {userName ? (
+            <>
+              <Avatar src="avatar.png" alt={userName} />
+              <span className="username-text">{userName}</span>
+              <Button
+                onClick={() => {
+                  localStorage.clear();
+                  history.push("/");
+                  window.location.reload();
+                }}
+              >
+                Logout
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button onClick={() => history.push("/login")}>Login</Button>
+              <Button
+                variant="contained"
+                onClick={() => history.push("/register")}
+              >
+                Register
+              </Button>
+            </>
+          )}
+        </Stack>
+      )}
+    </Box>
+  );
 };
-
 export default Header;
